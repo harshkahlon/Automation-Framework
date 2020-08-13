d7f527174099683ae59b9d32dc1d4789e31b5916
harshkahlon <harshkahlon@live.com>
2020-08-13 07:06:53 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 #CONSTANTS
 
 
-#ref1URL = "https://testwebportal.wellpartner.com/Security/Authentication/LoginForm"
-#prodURL = "https://webportal.wellpartner.com"
-#dev3 = "http://devwebportal.wellpartner.com:3/Security/Authentication/LoginForm"
+#ref1URL = "https://testwebportal"
+#prodURL = "https://"
+#dev3 = "http://"
 
 
 
-ref1URL =  "https://testwebportal.wellpartner.com/Security/Authentication/LoginForm"
+ref1URL =  "https://testwebportal"
 
 
-adminUSERNAME = "hkahlon"
-adminPASSWORD = "jan20@Uber"
-entityUSERNAME = "entityauto"
-entityPASSWORD = "Automation@20"
+adminUSERNAME = ""
+adminPASSWORD = ""
+entityUSERNAME = ""
+entityPASSWORD = ""
 
 
